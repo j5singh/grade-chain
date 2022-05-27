f78675e12fe8f609f1b642855f9409b2eeb2334b
j5singh <76665728+j5singh@users.noreply.github.com>
2022-05-27 20:21:07 +0200

    refactored code

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -2,12 +2,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 require('dotenv').config();
 
-//IMPORT MODELS
-const User = require('./models/user')
+// IMPORT MODELS
+const api = require('./routes/api')
 
 // Connecting to the MongoDB database
 mongoose
@@ -21,84 +19,11 @@ mongoose
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()) // To parse the incoming requests with JSON payloads
 
+// ROUTES
 app.get('/', (req, res) => {
-    res.send('Hello from API!');
+    res.send('Hello from the grade-chain backend! Server is set up successfully');
 })
 
-app.post('/api', async (req, res) => {
-    res.send({msg: 'Hello from API! You are ' + req.body.name + ' ' + req.body.surname})
-});
-
-app.post('/api/register', async (req, res) => {
-    const name = req.body.name
-    const surname = req.body.surname
-    const email = req.body.email
-    const password = req.body.password
-    const cryptedPass = await bcrypt.hash(password, 10)
-
-    // Could add .lean() at the end if you don't want the default values
-    const response = await User.findOne({email: email})
-
-    if (response) {
-        return res.send({ result_msg: "The inserted credentials seem to be already registered inside our database!", status: "ERROR", result_data: {} })
-    }
-
-    try {
-        creation = await User.create({
-            name: name, 
-            surname: surname,
-            email: email,
-            password: cryptedPass
-        })
-    } catch (error) {
-        return res.send({ result_msg: error.message, status: "ERROR", result_data: {} })
-    }
-
-    // If you reach till here, it means that the email is found in the records but the password is incorrect
-    return res.send({ result_msg: "Successfully created!", status: "SUCCESS", result_data: {} })
-});
-
-app.post('/api/login', async (req, res) => {
-    const password = req.body.password
-    const email = req.body.email
-
-    // Could add .lean() at the end if you don't want the default values
-    const response = await User.findOne({email: email})
-
-    if (!response) {
-        return res.send({ result_msg: "The inserted credentials do not seem to match any record on our database!", status: "ERROR", result_data: {} })
-    }
-
-    const token = jwt.sign({ 
-        email: response.email
-    }, process.env.JWT_SECRET, { expiresIn: 60 })
-
-    if (await bcrypt.compare(password, response.password)) {
-        await User.findByIdAndUpdate(response._id, { token: token })
-        return res.send({ result_msg: "OK", status: "SUCCESS", result_data: {token: token} })
-    }
-    // If you reach till here, it means that the email is found in the records but the password is incorrect
-    return res.send({ result_msg: "Invalid password!", status: "ERROR", result_data: {} })
-});
-
-app.post('/api/verifytoken', async (req, res) => {
-    const token = req.body.token
-
-    try {
-        const user = jwt.verify(token, process.env.JWT_SECRET)
-    } catch (error) {
-        return res.send({ result_msg: "Token Expired!", status: "ERROR", result_data: {} })
-    }
-
-    const response = await User.findOne({ token: token }) 
-
-    if (!response) {
-        return res.send({ result_msg: "The inserted credentials do not seem to match any record on our database!", status: "ERROR", result_data: {} })
-    }
-
-    return res.send({ result_msg: "OK", status: "SUCCESS", result_data: response })
-
-})
-
+app.use('/api', api);
 
 app.listen(process.env.PORT, () => console.log(`Server Running on: http://localhost:${process.env.PORT}`));
---
+++ back-end/src/routes/api.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
+
+// IMPORT MODELS
+const User = require('../models/user')
+
+// We are inside /api
+router.post('/', async (req, res) => {
+    res.send({msg: 'Hello from API! You are ' + (req.body.name || 'pippo') + ' ' + (req.body.surname || 'pluto')})
+});
+
+// Register route
+router.post('/register', async (req, res) => {
+    const name = req.body.name
+    const surname = req.body.surname
+    const email = req.body.email
+    const password = req.body.password
+    const cryptedPass = await bcrypt.hash(password, 10)
+
+    // Could add .lean() at the end if you don't want the default values
+    const response = await User.findOne({email: email})
+
+    if (response) {
+        return res.send({ result_msg: "The inserted credentials seem to be already registered inside our database!", status: "ERROR", result_data: {} })
+    }
+
+    try {
+        creation = await User.create({
+            name: name, 
+            surname: surname,
+            email: email,
+            password: cryptedPass
+        })
+    } catch (error) {
+        return res.send({ result_msg: error.message, status: "ERROR", result_data: {} })
+    }
+
+    // If you reach till here, it means that the email is found in the records but the password is incorrect
+    return res.send({ result_msg: "Successfully created!", status: "SUCCESS", result_data: {} })
+});
+
+// Login route
+router.post('/login', async (req, res) => {
+    const password = req.body.password
+    const email = req.body.email
+
+    // Could add .lean() at the end if you don't want the default values
+    const response = await User.findOne({email: email})
+
+    if (!response) {
+        return res.send({ result_msg: "The inserted credentials do not seem to match any record on our database!", status: "ERROR", result_data: {} })
+    }
+
+    const token = jwt.sign({
+        email: response.email
+    }, process.env.JWT_SECRET, { expiresIn: 60 })
+
+    if (await bcrypt.compare(password, response.password)) {
+        await User.findByIdAndUpdate(response._id, { token: token })
+        return res.send({ result_msg: "OK", status: "SUCCESS", result_data: {token: token} })
+    }
+    // If you reach till here, it means that the email is found in the records but the password is incorrect
+    return res.send({ result_msg: "Invalid password!", status: "ERROR", result_data: {} })
+});
+
+// Verify token route
+router.post('/verifytoken', async (req, res) => {
+    const token = req.body.token
+
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (error) {
+        return res.send({ result_msg: "Token Expired!", status: "ERROR", result_data: {} })
+    }
+
+    const response = await User.findOne({ token: token }) 
+
+    if (!response) {
+        return res.send({ result_msg: "The inserted credentials do not seem to match any record on our database!", status: "ERROR", result_data: {} })
+    }
+
+    return res.send({ result_msg: "OK", status: "SUCCESS", result_data: response })
+})
+
+module.exports = router
